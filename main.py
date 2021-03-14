b71ac40fc09d6c93b48a84a390eb3655965e598b
l-Il <0rky@pm.me>
2021-03-14 17:02:08 +0300

    + OOP

--- main.py
+++ main.py
@@ -3,162 +3,137 @@ from PIL import ImageTk
 from PIL import Image as Img
 from tkinter import *
 
-A = []
-filepath = 'bd.txt'
-texture = r'textures.png'
-try:
-    key = open("key.key", "rb").read()
-except FileNotFoundError:
-    with open(f'key.key', 'wb') as key_file:
-        key_file.write(Fernet.generate_key())
-    key = open(f'key.key', 'rb').read()
 
-f = Fernet(key)
-style = ['#202020', '#ffffff', '#6a8759']
+class Main:
+    def __init__(self):
+        self.A = []
+        self.filepath = 'bd.txt'
+        self.texture = r'textures.png'
+        self.style = ['#202020', '#ffffff', '#6a8759']
+        try:
+            self.key = open("key.key", "rb").read()
+        except FileNotFoundError:
+            with open(f'key.key', 'wb') as key_file:
+                key_file.write(Fernet.generate_key())
+            self.key = open(f'key.key', 'rb').read()
+        self.f = Fernet(self.key)
 
+        self.root = Tk()
+        self.root.title("Password Manager")
+        self.root.configure(bg=self.style[0])
 
-def every():
-    status['text'] = ''
-    entry.delete(0, END)
-    root.update()
+        self.img_add = ImageTk.PhotoImage(Img.open(self.texture).crop((1, 1, 17, 17)))
+        self.img_del = ImageTk.PhotoImage(Img.open(self.texture).crop((18, 1, 34, 17)))
+        self.img_chng = ImageTk.PhotoImage(Img.open(self.texture).crop((35, 1, 51, 17)))
+        self.img_exit = ImageTk.PhotoImage(Img.open(self.texture).crop((52, 1, 68, 17)))
+        self.img_ent = ImageTk.PhotoImage(Img.open(self.texture).crop((69, 1, 85, 17)))
 
+        self.taskbar = Frame(bg=self.style[0])
+        self.taskbar.pack(side=LEFT, fill=Y)
+        Button(self.taskbar, text='Добавить', fg=self.style[1], image=self.img_add, compound="left", relief=FLAT, bg=self.style[0], command=self.add).pack(expand=1, anchor=NW)
+        Button(self.taskbar, text='Изменить', fg=self.style[1], image=self.img_chng, compound="left", relief=FLAT, bg=self.style[0], command=self.edit).pack(expand=1, anchor=NW)
+        Button(self.taskbar, text='Удалить', fg=self.style[1], image=self.img_del, compound="left", relief=FLAT, bg=self.style[0], command=self.delete).pack(expand=1, anchor=NW)
+        Button(self.taskbar, text='Выход', fg=self.style[1], image=self.img_exit, compound="left", relief=FLAT, bg=self.style[0], command=self.close).pack(expand=1, anchor=NW)
 
-def add():
-    status['text'] = '[Добавление] Введите задачу: '
-    entry.delete(0, END)
-    root.wait_variable(variable)
-    if entry.get() == '':
-        status['text'] = 'Ошибка. Пустой ввод. Выхожу в главное меню:'
-    else:
-        listbox.insert(END, entry.get())
-        entry.delete(0, END)
-        every()
+        self.toolbar = Frame(bg=self.style[0])
+        self.toolbar.pack(side=TOP, fill=X)
 
+        self.top_frame = Frame(self.toolbar, bg=self.style[0])
+        self.top_frame.pack(side=TOP, fill=X)
+        self.status = Label(self.top_frame, fg=self.style[2], width=63, height=1, bg=self.style[0])
+        self.status.pack(side=LEFT, fill=X)
+        self.variable = IntVar()
+        self.send_btn = Button(self.top_frame, image=self.img_ent, relief=FLAT, bg=self.style[0], command=lambda: self.variable.set(1))
+        self.send_btn.pack(side=RIGHT)
+        self.entry = Entry(self.top_frame, fg=self.style[1], bg=self.style[0], relief=FLAT)
+        self.entry.pack(side=RIGHT)
 
-def edit():
-    status['text'] = '[Изменение] Выберите задачу, введите и нажмите клавишу.'
-    root.wait_variable(variable)
-    try:
-        i = listbox.curselection()[0]
-        listbox.delete(i)
-        listbox.insert(i, entry.get())
-        entry.delete(0, END)
-        every()
-    except IndexError:
-        status['text'] = 'Ошибка. Не выбрана задача. Выхожу в главное меню.'
+        self.bottom_frame = Frame(self.toolbar, bg=self.style[0])
+        self.bottom_frame.pack(side=TOP, fill=X)
+        self.listbox = Listbox(self.bottom_frame, fg=self.style[1], bg=self.style[0], width=96, height=10)
+        self.listbox.pack(side=LEFT, fill=X, anchor='s')
+        self.scrollbar = Scrollbar(self.bottom_frame)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.scrollbar.config(command=self.listbox.yview)
+        self.listbox.config(yscrollcommand=self.scrollbar.set)
 
+        self.root.mainloop()
+        self.load()
 
-def delete():
-    status['text'] = '[Удаление] Выберите задачу и нажмите клавишу.'
-    entry.delete(0, END)
-    root.wait_variable(variable)
-    try:
-        i = listbox.curselection()[0]
-        listbox.delete(i)
-        every()
-    except IndexError:
-        status['text'] = 'Ошибка. Не выбрана задача. Выхожу в главное меню.'
+    def every(self):
+        self.status['text'] = ''
+        self.entry.delete(0, END)
+        self.root.update()
 
+    def add(self):
+        self.status['text'] = '[Добавление] Введите задачу: '
+        self.entry.delete(0, END)
+        self.root.wait_variable(self.variable)
+        if self.entry.get() == '':
+            self.status['text'] = 'Ошибка. Пустой ввод. Выхожу в главное меню:'
+        else:
+            self.listbox.insert(END, self.entry.get())
+            self.entry.delete(0, END)
+            self.every()
 
-def load():
-    global A
-    try:
-        with open(filepath, "rb") as file:
-            decrypted_data = f.decrypt(file.read())
-        with open(filepath, "wb") as file:
-            file.write(decrypted_data)
-        with open(filepath, 'r', encoding='UTF-8') as file:
-            listbox.delete(0, END)
-            for i in file:
-                i.strip()
-                A.append(i.replace('\n', '').replace('▓▓', '▓'))
-            A = sorted(A)
-            for i in A:
-                listbox.insert(END, i)
-    except FileNotFoundError:
-        status['text'] = 'Ошибка. Не могу найти файл. Запуск.'
-    except (InvalidToken, TypeError):
-        save()
-        load()
+    def edit(self):
+        self.status['text'] = '[Изменение] Выберите задачу, введите и нажмите клавишу.'
+        self.root.wait_variable(self.variable)
+        try:
+            i = self.listbox.curselection()[0]
+            self.listbox.delete(i)
+            self.listbox.insert(i, self.entry.get())
+            self.entry.delete(0, END)
+            self.every()
+        except IndexError:
+            self.status['text'] = 'Ошибка. Не выбрана задача. Выхожу в главное меню.'
 
+    def delete(self):
+        self.status['text'] = '[Удаление] Выберите задачу и нажмите клавишу.'
+        self.entry.delete(0, END)
+        self.root.wait_variable(self.variable)
+        try:
+            i = self.listbox.curselection()[0]
+            self.listbox.delete(i)
+            self.every()
+        except IndexError:
+            self.status['text'] = 'Ошибка. Не выбрана задача. Выхожу в главное меню.'
 
+    def load(self):
+        try:
+            with open(self.filepath, "rb") as file:
+                decrypted_data = self.f.decrypt(file.read())
+            with open(self.filepath, "wb") as file:
+                file.write(decrypted_data)
+            with open(self.filepath, 'r', encoding='UTF-8') as file:
+                self.listbox.delete(0, END)
+                for i in file:
+                    i.strip()
+                    self.A.append(i.replace('\n', '').replace('▓▓', '▓'))
+                self.A = sorted(self.A)
+                for i in self.A:
+                    self.listbox.insert(END, i)
+        except FileNotFoundError:
+            self.status['text'] = 'Ошибка. Не могу найти файл. Запуск.'
+        except (InvalidToken, TypeError):
+            self.save()
+            self.load()
 
-def save():
-    global A
-    A = list(listbox.get(0, listbox.size()))
-    with open(filepath, 'w', encoding='UTF-8') as file:
-        for i in range(len(A)):
-            file.write(str(A[i]) + '\n')
-    with open(filepath, "rb") as file:
-        encrypted_data = f.encrypt(file.read())
-    with open(filepath, "wb") as file:
-        file.write(encrypted_data)
+    def save(self):
+        self.A = list(self.listbox.get(0, self.listbox.size()))
+        with open(self.filepath, 'w', encoding='UTF-8') as file:
+            for i in range(len(self.A)):
+                file.write(str(self.A[i]) + '\n')
+        with open(self.filepath, "rb") as file:
+            encrypted_data = self.f.encrypt(file.read())
+        with open(self.filepath, "wb") as file:
+            file.write(encrypted_data)
 
-
-def close():
-    save()
-    every()
-    print('Выход...')
-    raise SystemExit
-
-
-class Main(Frame):
-    def __init__(self, root):
-        super().__init__(root)
-        self.icons()
-        self.taskbar()
-        self.data()
-
-    def icons(self):
-        self.img_add = ImageTk.PhotoImage(Img.open(texture).crop((1, 1, 17, 17)))
-        self.img_chng = ImageTk.PhotoImage(Img.open(texture).crop((18, 1, 34, 17)))
-        self.img_del = ImageTk.PhotoImage(Img.open(texture).crop((35, 1, 51, 17)))
-        self.img_exit = ImageTk.PhotoImage(Img.open(texture).crop((52, 1, 68, 17)))
-        self.img_ent = ImageTk.PhotoImage(Img.open(texture).crop((69, 1, 85, 17)))
-
-    def taskbar(self):
-        taskbar = Frame(bg=style[0])
-        taskbar.pack(side=LEFT, fill=Y)
-        Button(taskbar, text='Добавить', fg=style[1], image=self.img_add, compound="left", relief=FLAT, bg=style[0],
-               command=add).pack(expand=1, anchor=NW)
-        Button(taskbar, text='Изменить', fg=style[1], image=self.img_chng, compound="left", relief=FLAT, bg=style[0],
-               command=edit).pack(expand=1, anchor=NW)
-        Button(taskbar, text='Удалить', fg=style[1], image=self.img_del, compound="left", relief=FLAT, bg=style[0],
-               command=delete).pack(expand=1, anchor=NW)
-        Button(taskbar, text='Выход', fg=style[1], image=self.img_exit, compound="left", relief=FLAT, bg=style[0],
-               command=close).pack(expand=1, anchor=NW)
-
-    def data(self):
-        global status
-        global entry
-        global listbox
-        global variable
-        toolbar = Frame(bg=style[0])
-        toolbar.pack(side=TOP, fill=X)
-
-        top_frame = Frame(toolbar, bg=style[0])
-        top_frame.pack(side=TOP, fill=X)
-        status = Label(top_frame, fg=style[2], width=63, height=1, bg=style[0])
-        status.pack(side=LEFT, fill=X)
-        variable = IntVar()
-        send_btn = Button(top_frame, image=self.img_ent, relief=FLAT, bg=style[0], command=lambda: variable.set(1))
-        send_btn.pack(side=RIGHT)
-        entry = Entry(top_frame, fg=style[1], bg=style[0], relief=FLAT)
-        entry.pack(side=RIGHT)
-
-        bottom_frame = Frame(toolbar, bg=style[0])
-        bottom_frame.pack(side=TOP, fill=X)
-        listbox = Listbox(bottom_frame, fg=style[1], bg=style[0], width=96, height=10)
-        listbox.pack(side=LEFT, fill=X, anchor='s')
-        scrollbar = Scrollbar(bottom_frame)
-        scrollbar.pack(side=RIGHT, fill=Y)
-        scrollbar.config(command=listbox.yview)
-        listbox.config(yscrollcommand=scrollbar.set)
+    def close(self):
+        self.save()
+        self.every()
+        print('Выход...')
+        raise SystemExit
 
 
-root = Tk()
-root.title("Password Manager")
-root.configure(bg=style[0])
-app = Main(root)
-load()
-root.mainloop()
+_ = Main()
